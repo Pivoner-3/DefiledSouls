cffe273ac8b071dc7edbf6b5ee8e3f036a89ca49
Пивонер <nikguz7@mail.ru>
2025-11-28 10:25:13 +0500

    Create Dialog

---
+++ Assets/Scenes/Dialogue/DialogSystem.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour
+{
+    public GameObject windowDialog;
+    public TextMeshProUGUI textDialog;
+    public string[] message;
+    public int numberDialog = 0;
+    public Button button;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if(numberDialog == message.Length - 1)
+            {
+                button.gameObject.SetActive(false);
+            }
+            else
+            {
+                button.gameObject.SetActive(true);
+                button.onClick.AddListener(NextDialog);
+            }
+            windowDialog.SetActive(true);
+            textDialog.text = message[numberDialog];
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        windowDialog.SetActive(false);
+        numberDialog = 0;
+        button.onClick.RemoveAllListeners();
+    }
+    public void NextDialog()
+    {
+        numberDialog++;
+        textDialog.text = message[numberDialog];
+        if (numberDialog == message.Length - 1)
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+}
